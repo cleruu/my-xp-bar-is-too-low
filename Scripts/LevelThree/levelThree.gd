6dec62e59a87f6fff7ab14815d0e381836b3ac71
Dhan Tamparong <111488321+dhan-t@users.noreply.github.com>
2026-07-15 19:53:03 +0800

    goal logic connected
    simple print statement palang

--- Scripts/LevelThree/levelThree.gd
+++ Scripts/LevelThree/levelThree.gd
@@ -6,8 +6,17 @@ func _ready() -> void:
 	for guard in guards:
 		guard.player_caught.connect(_on_player_caught)
 
+	var goal = get_tree().get_first_node_in_group("goal")
+	if goal:
+		goal.player_reached_goal.connect(_on_player_reached_goal)
+	else:
+		print("LevelThree: no goal zone found in 'goal' group!")
+
 func _on_player_caught() -> void:
 	print("LevelThree: received player_caught signal, reloading")
 	var error = get_tree().reload_current_scene()
 	if error != OK:
 		print("RELOAD FAILED with error code: ", error)
+
+func _on_player_reached_goal() -> void:
+	print("LevelThree: player reached the goal! YOU WIN")
