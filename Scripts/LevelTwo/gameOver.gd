cdb852e231aac295ee1a829baa60c29cc47474b8
Jasper Maraño <Clerumonogatari65@gmail.com>
2026-07-17 00:48:23 +0800

    fixed l2 retry

--- Scripts/LevelTwo/gameOver.gd
+++ Scripts/LevelTwo/gameOver.gd
@@ -4,4 +4,4 @@ func _on_restart_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/LevelTwo/levelTwo.tscn")
 
 func _on_quit_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Start/Starting Screen.tscn")
+	get_tree().change_scene_to_file("res://Scenes/LevelTwo/levelTwo.tscn")
