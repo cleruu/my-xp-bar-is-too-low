52499b3ec99ba0eed30402a7e213445dd5d251bd
Jasper Maraño <Clerumonogatari65@gmail.com>
2026-07-16 23:48:52 +0800

    final ver

--- Scripts/End/Manager End Cutscene.gd
+++ Scripts/End/Manager End Cutscene.gd
@@ -6,13 +6,13 @@ var atEnd := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	if Input.is_action_just_pressed("Next"):
-		if atEnd:
-			get_tree().change_scene_to_file(endCutscene)
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("Next"):
+		if atEnd:
+			get_tree().change_scene_to_file(endCutscene)
 
 func isAtEnd() -> void:
 	atEnd = true
