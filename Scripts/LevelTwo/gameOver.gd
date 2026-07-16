55b9e3b47059dfc08fd6b75f90b014e0b4cfc370
Jasper Maraño <Clerumonogatari65@gmail.com>
2026-07-17 00:54:47 +0800

    test this shit

--- Scripts/LevelTwo/gameOver.gd
+++ Scripts/LevelTwo/gameOver.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
+@export_file("*.tscn") var endCutscene: String
+
 func _on_restart_button_pressed():
 	get_tree().change_scene_to_file("res://Scenes/LevelTwo/levelTwo.tscn")
 
 func _on_quit_button_pressed():
-	get_tree().change_scene_to_file("res://Scenes/LevelTwo/levelTwo.tscn")
+	get_tree().change_scene_to_file(endCutscene)
