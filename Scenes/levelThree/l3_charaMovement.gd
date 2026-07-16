34f36d1b98ce7aaeb0bced35ad1db61d2e25439f
Robbie Espaldon <espaldonrobbie88@gmail.com>
2026-07-16 12:32:00 +0800

    debug: add footstep SFX debug prints

--- Scenes/levelThree/l3_charaMovement.gd
+++ Scenes/levelThree/l3_charaMovement.gd
@@ -11,6 +11,11 @@ func _ready() -> void:
 	footstep_stream = preload("res://Assets/Sounds/SFX/Walking Sound Effects.wav")
 	footstep_stream.loop_mode = AudioStreamWAV.LOOP_FORWARD
 	footstep_sfx.stream = footstep_stream
+	print("ROBS_DEBUG: FootstepSFX node: ", footstep_sfx != null)
+	print("ROBS_DEBUG: Footstep stream set: ", footstep_sfx.stream != null)
+	print("ROBS_DEBUG: Footstep stream length: ", footstep_stream.get_length())
+	print("ROBS_DEBUG: Footstep volume_db: ", footstep_sfx.volume_db)
+	footstep_sfx.volume_db = 0.0
 
 func _physics_process(delta):
 	character_direction.x = Input.get_axis("moveLeft", "moveRight")
@@ -30,6 +35,7 @@ func _physics_process(delta):
 		if %charaSprite.animation != "walk_r": %charaSprite.animation = "walk_r"
 		if not footstep_sfx.playing:
 			footstep_sfx.play()
+			print("ROBS_DEBUG: Footstep play() called, playing: ", footstep_sfx.playing)
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, movement_speed)
 		if %charaSprite.animation != "idle": %charaSprite.animation = "idle"
